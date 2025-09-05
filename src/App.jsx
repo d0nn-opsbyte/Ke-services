bccf7a83c6b345cab26e4fa808667e69eaad9be2
donald kiarie <donaldkiarie768@gmail.com>
2025-09-05 21:51:01 +0300

    more changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import Signup from "./components/Signup";
 import Dashboard from "./components/Dashboard";
 import Services from "./components/Services";
 import Wallet from "./components/Wallet";
-import Orders from "./components/Orders";
 import Profile from "./components/Profile";
+import Orders from "./components/Orders";
 import "./App.css";
 
 function App() {
@@ -16,11 +16,13 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+
+        {/* ✅ Pass user id as param */}
         <Route path="/dashboard/:id" element={<Dashboard />} />
         <Route path="/services/:id" element={<Services />} />
         <Route path="/wallet/:id" element={<Wallet />} />
-        <Route path="/orders/:id" element={<Orders />} />
         <Route path="/profile/:id" element={<Profile />} />
+        <Route path="/orders/:id" element={<Orders />} />
       </Routes>
     </Router>
   );
